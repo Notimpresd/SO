2ba4ab43e8471de3822d6d23d9659e98e075fa80
ionut <ionut.costean@continental-corporation.com>
2025-04-07 17:40:47 +0300

    week 6 update

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <errno.h>
 
-typedef struct {
+#define LOG_FILE "logged_hunt"
+
+typedef struct
+{
     int ID;
     char user_name[20];
     float GPS_longitude;
@@ -15,46 +18,44 @@ typedef struct {
 
 void add_treasure(char *hunt_id)//add
 {
-    printf("Treasure added to hunt %s",hunt_id);
+    printf("\nTreasure added to hunt %s.\n",hunt_id);
 }
 
 void list_treasures(char *hunt_id)//list
 {
-    printf("Treasures listed for hunt %s",hunt_id);
-
+    printf("\nTreasures listed for hunt %s.\n",hunt_id);
 }
 
 void view_treasure(char *hunt_id, int id)//view
 {
-    printf("Treasure %d viewed for %s",id,hunt_id);
-
+    printf("\nTreasure %d viewed for %s.\n",id,hunt_id);
 }
 
 void remove_treasure(char *hunt_id, int id)//remove_treasure
 {
-    printf("Treasure %d removed to hunt %s",id,hunt_id);
-
+    printf("\nTreasure %d removed from hunt %s.\n",id,hunt_id);
 }
 
 void remove_hunt(char *hunt_id)//remove_hunt
 {
-    printf("Hunt removed from hunt %s",hunt_id);
-
+    printf("\nHunt %s removed.\n",hunt_id);
 }
 
 void log_operation(char *hunt_id, char *operation)//log
 {
-    printf("Operation %s from hunt %s log.",operation,hunt_id);
-
+    printf("Operation \"%s\" from hunt %s has been recorded in log file.\n",operation,hunt_id);
 }
 
 void create_symlink(char *hunt_id)//create_symlink
 {
-    printf("Symlink for hunt %s created.",hunt_id);
+    printf("\nSymlink for hunt %s created.\n",hunt_id);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
         printf("Not enough arguments.\n");
         exit(EXIT_FAILURE);
     }
@@ -62,20 +63,32 @@ int main(int argc, char *argv[]) {
     char *operation = argv[1];
     char *hunt_id = argv[2];
 
-    if (strcmp(operation, "add") == 0) {
+    if(strcmp(operation, "add") == 0)
+    {
         add_treasure(hunt_id);
-    } else if (strcmp(operation, "list") == 0) {
+        log_operation(hunt_id,operation);
+    }
+    else if(strcmp(operation, "list") == 0)
+    {
         list_treasures(hunt_id);
-    } else if (strcmp(operation, "view") == 0 && argc == 4) {
+    }
+    else if(strcmp(operation, "view") == 0 && argc == 4)
+    {
         int id = atoi(argv[3]);
         view_treasure(hunt_id, id);
-    } else if (strcmp(operation, "remove_treasure") == 0 && argc == 4) {
+    }
+    else if (strcmp(operation, "remove_treasure") == 0 && argc == 4)
+    {
         int id = atoi(argv[3]);
         remove_treasure(hunt_id, id);
-    } else if (strcmp(operation, "remove_hunt") == 0) {
+    }
+    else if (strcmp(operation, "remove_hunt") == 0)
+    {
         remove_hunt(hunt_id);
-    } else {
-        printf("Invalid arguments\n");
+    }
+    else
+    {
+        printf("Invalid arguments.\n");
         exit(EXIT_FAILURE);
     }
 
